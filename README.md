12dc9d3f3c144aeac0d7a07fa981073d8f0340d8
buyekeobare <buyekeobare@gmail.com>
2024-01-31 14:20:41 +0300

    Weather App files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weather App
+
+## Technologies Used:
+
+Vanilla:
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Feature #1
+
+In your project, display the current date and time using JavaScript: Tuesday 16:00
+
+## 🕵️‍♀️Feature #2
+
+Add a search engine: a search bar with a button. When searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
---
+++ src/index.js
@@ -0,0 +1,46 @@
+// Challenge 1 
+
+let currentTime = new Date();
+console.log(currentTime);
+
+let day = currentTime.getDay();
+console.log(day);
+
+ let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let today = days[currentTime.getDay()];
+
+  let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentDay = document.querySelector("#current-day");
+currentDay.innerHTML = `${today} ${hours}: ${minutes}`;
+
+//Challenge 2
+
+function searchCity(event) {
+  event.preventDefault();
+  let currentCity = document.querySelector(".current-city");
+  let searchInput = document.querySelector(".search-input");
+  currentCity.innerHTML = searchInput.value;
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchCity);
+
+
+
